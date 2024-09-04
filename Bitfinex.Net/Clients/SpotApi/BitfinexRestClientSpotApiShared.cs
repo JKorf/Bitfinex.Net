e8932f26c32a53900436ca223bde6fc4d4e2179f
Jkorf <jankorf91@gmail.com>
2024-09-04 14:53:26 +0200

    wip

--- Bitfinex.Net/Clients/SpotApi/BitfinexRestClientSpotApiShared.cs
+++ Bitfinex.Net/Clients/SpotApi/BitfinexRestClientSpotApiShared.cs
@@ -224,7 +224,7 @@ namespace Bitfinex.Net.Clients.SpotApi
         }
 
         EndpointOptions ISpotTickerRestClient.GetSpotTickersOptions { get; } = new EndpointOptions("GetSpotTickersRequest", false);
-        async Task<ExchangeWebResult<IEnumerable<SharedSpotTicker>>> ISpotTickerRestClient.GetSpotTickersAsync(ApiType? apiType, ExchangeParameters? exchangeParameters, CancellationToken ct)
+        async Task<ExchangeWebResult<IEnumerable<SharedSpotTicker>>> ISpotTickerRestClient.GetSpotTickersAsync(ExchangeParameters? exchangeParameters, CancellationToken ct)
         {
             var validationError = ((ISpotTickerRestClient)this).GetSpotTickersOptions.ValidateRequest(Exchange, exchangeParameters);
             if (validationError != null)
