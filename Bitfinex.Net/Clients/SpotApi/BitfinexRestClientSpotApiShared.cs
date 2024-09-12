053f24720066807ca50e43c3d2d8e41f23f42603
JKorf <jankorf91@gmail.com>
2024-09-12 22:16:34 +0200

    wip

--- Bitfinex.Net/Clients/SpotApi/BitfinexRestClientSpotApiShared.cs
+++ Bitfinex.Net/Clients/SpotApi/BitfinexRestClientSpotApiShared.cs
@@ -312,10 +312,10 @@ namespace Bitfinex.Net.Clients.SpotApi
                 SharedTimeInForce.FillOrKill
             },
             new SharedQuantitySupport(
-                SharedQuantityType.BaseAssetQuantity,
-                SharedQuantityType.BaseAssetQuantity,
-                SharedQuantityType.BaseAssetQuantity,
-                SharedQuantityType.BaseAssetQuantity));
+                SharedQuantityType.BaseAsset,
+                SharedQuantityType.BaseAsset,
+                SharedQuantityType.BaseAsset,
+                SharedQuantityType.BaseAsset));
 
         async Task<ExchangeWebResult<SharedId>> ISpotOrderRestClient.PlaceSpotOrderAsync(PlaceSpotOrderRequest request, ExchangeParameters? exchangeParameters, CancellationToken ct)
         {
