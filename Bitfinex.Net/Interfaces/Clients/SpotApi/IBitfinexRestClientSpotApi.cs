c57a161236efc57d386833ec22f01d5b28c759c4
Jkorf <jankorf91@gmail.com>
2024-09-25 16:29:10 +0200

    wip

--- Bitfinex.Net/Interfaces/Clients/SpotApi/IBitfinexRestClientSpotApi.cs
+++ Bitfinex.Net/Interfaces/Clients/SpotApi/IBitfinexRestClientSpotApi.cs
@@ -25,9 +25,8 @@ namespace Bitfinex.Net.Interfaces.Clients.SpotApi
         public IBitfinexRestClientSpotApiTrading Trading { get; }
 
         /// <summary>
-        /// Get the ISpotClient for this client. This is a common interface which allows for some basic operations without knowing any details of the exchange.
+        /// DEPRECATED; use <see cref="CryptoExchange.Net.SharedApis.ISharedClient" /> instead for common/shared functionality. See <see href="SHAREDDOCSURL" /> for more info.
         /// </summary>
-        /// <returns></returns>
         public ISpotClient CommonSpotClient { get; }
 
         /// <summary>
