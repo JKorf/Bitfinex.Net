ed8bac874730c3ba4bf34621ded1f94c5f12a820
Jkorf <jankorf91@gmail.com>
2025-08-22 14:54:23 +0200

    Added ClearUserClients method to user client provider

--- Bitfinex.Net/Clients/BitfinexUserClientProvider.cs
+++ Bitfinex.Net/Clients/BitfinexUserClientProvider.cs
@@ -54,6 +54,13 @@ namespace Bitfinex.Net.Clients
         }
 
         /// <inheritdoc />
+        public void ClearUserClients(string userIdentifier)
+        {
+            _restClients.TryRemove(userIdentifier, out _);
+            _socketClients.TryRemove(userIdentifier, out _);
+        }
+
+        /// <inheritdoc />
         public IBitfinexRestClient GetRestClient(string userIdentifier, ApiCredentials? credentials = null, BitfinexEnvironment? environment = null)
         {
             if (!_restClients.TryGetValue(userIdentifier, out var client))
