b49c7cef3b91cdeda978b5902262a112204956ca
Jkorf <jankorf91@gmail.com>
2026-03-20 13:57:26 +0100

    wip

--- Bitfinex.Net/ExtensionMethods/ServiceCollectionExtensions.cs
+++ Bitfinex.Net/ExtensionMethods/ServiceCollectionExtensions.cs
@@ -22,7 +22,7 @@ namespace Microsoft.Extensions.DependencyInjection
     {
         /// <summary>
         /// Add services such as the IBitfinexRestClient and IBitfinexSocketClient. Configures the services based on the provided configuration.<br />
-        /// See <see href="https://github.com/JKorf/CryptoExchange.Net/blob/master/Examples/example-config.json" /> for an example of how to set up the configuration.
+        /// See <see href="https://github.com/JKorf/Bitfinex.Net/blob/master/Examples/example-config.json" /> for an example of how to set up the configuration.
         /// </summary>
         /// <param name="services">The service collection</param>
         /// <param name="configuration">The configuration(section) containing the options</param>
