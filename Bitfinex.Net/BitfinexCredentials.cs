bf1f55bfac8c327ec7aadca4d1e9debd95ce6bb1
Jkorf <jankorf91@gmail.com>
2026-03-24 16:31:33 +0100

    Fix issue in credentials copying

--- Bitfinex.Net/BitfinexCredentials.cs
+++ Bitfinex.Net/BitfinexCredentials.cs
@@ -43,5 +43,8 @@ namespace Bitfinex.Net
             Secret = secret;
             return this;
         }
+
+        /// <inheritdoc />
+        public override ApiCredentials Copy() => new BitfinexCredentials(this);
     }
 }
