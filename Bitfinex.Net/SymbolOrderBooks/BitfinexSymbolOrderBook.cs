57db295f57da449436924d015d47a436db8273fa
JKorf <jankorf91@gmail.com>
2026-04-13 17:53:05 +0200

    Fixed subscription not getting closed when BitfinexSymbolOrderBook start fails

--- Bitfinex.Net/SymbolOrderBooks/BitfinexSymbolOrderBook.cs
+++ Bitfinex.Net/SymbolOrderBooks/BitfinexSymbolOrderBook.cs
@@ -84,6 +84,9 @@ namespace Bitfinex.Net.SymbolOrderBooks
             Status = OrderBookStatus.Syncing;
 
             var setResult = await WaitForSetOrderBookAsync(_initialDataTimeout, ct).ConfigureAwait(false);
+            if (!setResult)
+                await result.Data.CloseAsync().ConfigureAwait(false);
+
             return setResult ? result : new CallResult<UpdateSubscription>(setResult.Error!);
         }
 
