0f768358088a65f100cd0581e7043ac86e57bc8d
Jkorf <jankorf91@gmail.com>
2026-09-22 15:37:10 +0200

    wip

--- Bitfinex.Net/Clients/ExchangeApi/Rest/SharedApi/BitfinexRestClientExchangeApiShared.FuturesSymbols.cs
+++ Bitfinex.Net/Clients/ExchangeApi/Rest/SharedApi/BitfinexRestClientExchangeApiShared.FuturesSymbols.cs
@@ -20,7 +20,7 @@ namespace Bitfinex.Net.Clients.ExchangeApi
 
         public SharedSymbolCatalog? FuturesSymbolCatalog => ExchangeSymbolCache.GetSymbolCatalog(_exchangeName, _topicFuturesId, _api.EnvironmentName, null);
         public GetFuturesSymbolsOptions GetFuturesSymbolsOptions { get; } = new GetFuturesSymbolsOptions(_exchangeName, false);
-        async Task<ICallResult<SharedFuturesSymbol[]>> IGetFuturesSymbols.GetFuturesSymbolsAsync(GetSymbolsRequest request, CancellationToken ct)
+        async Task<IExchangeCallResult<SharedFuturesSymbol[]>> IGetFuturesSymbols.GetFuturesSymbolsAsync(GetSymbolsRequest request, CancellationToken ct)
             => await GetFuturesSymbolsAsync(request, ct).ConfigureAwait(false);
 
         public async Task<HttpResult<SharedFuturesSymbol[]>> GetFuturesSymbolsAsync(GetSymbolsRequest request, CancellationToken ct)
--- Bitfinex.Net/Clients/ExchangeApi/Rest/SharedApi/BitfinexRestClientExchangeApiShared.SpotSymbols.cs
+++ Bitfinex.Net/Clients/ExchangeApi/Rest/SharedApi/BitfinexRestClientExchangeApiShared.SpotSymbols.cs
@@ -20,7 +20,7 @@ namespace Bitfinex.Net.Clients.ExchangeApi
 
         public SharedSymbolCatalog? SpotSymbolCatalog => ExchangeSymbolCache.GetSymbolCatalog(_exchangeName, _topicSpotId, _api.EnvironmentName, null);
         public GetSpotSymbolsOptions GetSpotSymbolsOptions { get; } = new GetSpotSymbolsOptions(_exchangeName, false);
-        async Task<ICallResult<SharedSpotSymbol[]>> IGetSpotSymbols.GetSpotSymbolsAsync(GetSymbolsRequest request, CancellationToken ct)
+        async Task<IExchangeCallResult<SharedSpotSymbol[]>> IGetSpotSymbols.GetSpotSymbolsAsync(GetSymbolsRequest request, CancellationToken ct)
             => await GetSpotSymbolsAsync(request, ct).ConfigureAwait(false);
 
         public async Task<HttpResult<SharedSpotSymbol[]>> GetSpotSymbolsAsync(GetSymbolsRequest request, CancellationToken ct)
